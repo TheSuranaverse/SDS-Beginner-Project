016c3eff7654ffdb240279e93a9735120aaa10f1
Subham <56317368+Neo-TheCondor@users.noreply.github.com>
2021-03-05 18:59:49 +0530

    Add files via upload

---
+++ Fake News Detection.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# In[10]:
+
+
+#Read the data
+df=pd.read_csv("D:\\Jupyter\\Untitled Folder\\news.csv")
+
+#Get shape and head
+df.shape
+df.head()
+
+
+# In[11]:
+
+
+#DataFlair - Get the labels
+labels=df.label
+labels.head()
+
+
+# In[12]:
+
+
+#DataFlair - Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+
+# In[13]:
+
+
+#DataFlair - Initialize a TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+#DataFlair - Fit and transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+
+# In[14]:
+
+
+#DataFlair - Initialize a PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+#DataFlair - Predict on the test set and calculate accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+
+# In[15]:
+
+
+#DataFlair - Build confusion matrix
+confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
+
+
+# In[ ]:
+
+
+
+
